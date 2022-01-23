c029227cdb5a8774c1825a6afdbe2276447bdd8a
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2022-01-23 21:22:22 +0700

    victory chart library

--- components/PriceChart.js
+++ components/PriceChart.js
@@ -3,8 +3,13 @@ import {View, Text, StyleSheet, SafeAreaView, FlatList, ActivityIndicator} from 
 import { NavigationContainer } from '@react-navigation/native';
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { useEffect, useState } from 'react';
+import { VictoryBar, VictoryChart, VictoryGroup, VictoryLine } from 'victory-native';
 
 const PorkURL = "https://dataapi.moc.go.th/gis-product-prices?product_id=P11001&from_date=2021-12-01&to_date=2023-12-31"
+
+const barData = {
+    minPrice: [{x: "date1", y: 200}, {x: "date2", y: 250}, {x: "date3", y: 180}, {x: "date4", y: 220}]
+}
 
 const PriceChart = ({navigation}) => {
 
@@ -27,7 +32,24 @@ const PriceChart = ({navigation}) => {
         <SafeAreaView>
             {isLoading ? <ActivityIndicator/> : 
             (
-                <View>
+                <ScrollView>
+
+                    <VictoryChart>
+                        <VictoryGroup>
+                            <VictoryLine data={barData.minPrice}>
+
+                            </VictoryLine>
+                        </VictoryGroup>
+                    </VictoryChart>
+
+                    <VictoryChart>
+                        <VictoryGroup>
+                            <VictoryBar data={barData.minPrice}>
+                                
+                            </VictoryBar>
+                        </VictoryGroup>
+                    </VictoryChart>
+
                     <Text>{title}</Text>
                         <View>
                             <FlatList
@@ -41,7 +63,7 @@ const PriceChart = ({navigation}) => {
                             )}
                             />
                         </View>  
-                </View>
+                </ScrollView>
             )}
             
         </SafeAreaView>
